c70e213f0fcce253cbc20822f771a6f4f45d0cda
Julien Leicher <jleicher@gmail.com>
2015-08-18 11:05:49 +0200

    Moved basic components to their own repo

--- FBlock.UnitTests/CoreTests.cs
+++ FBlock.UnitTests/CoreTests.cs
@@ -1,43 +1,10 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using FBlock.Core;
+using FBlock.UnitTests.Fixtures;
 
-namespace FBlock.UnitTests.Core
+namespace FBlock.UnitTests
 {
-    public class DummyComponent1 : Component<string, string>
-    {
-        protected int _value;
-
-        public DummyComponent1(int value = 0)
-        {
-            _value = value;
-        }
-
-        public override string Process(string arg, JobContext context)
-        {
-            if(_value != 0)
-                context.Set("DUMMY_VALUE", _value);
-
-            return context.Job.Name;
-        }
-    }
-
-    public class DummyComponent2 : Component<string, int>
-    {
-        public override int Process(string arg, JobContext context)
-        {
-            return context.Get<int>("DUMMY_VALUE");
-        }
-    }
-
-    public class DummyComponent3 : Component<int, string>
-    {
-        public override string Process(int arg, JobContext context)
-        {
-            return arg.ToString().ToUpper();
-        }
-    }
-
     [TestClass]
     public class CoreTests
     {
---
+++ FBlock.UnitTests/Fixtures/Components.cs
@@ -0,0 +1,43 @@
+﻿using FBlock.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FBlock.UnitTests.Fixtures
+{
+    public class DummyComponent1 : Component<string, string>
+    {
+        protected int _value;
+
+        public DummyComponent1(int value = 0)
+        {
+            _value = value;
+        }
+
+        public override string Process(string arg, JobContext context)
+        {
+            if (_value != 0)
+                context.Set("DUMMY_VALUE", _value);
+
+            return context.Job.Name;
+        }
+    }
+
+    public class DummyComponent2 : Component<string, int>
+    {
+        public override int Process(string arg, JobContext context)
+        {
+            return context.Get<int>("DUMMY_VALUE");
+        }
+    }
+
+    public class DummyComponent3 : Component<int, string>
+    {
+        public override string Process(int arg, JobContext context)
+        {
+            return arg.ToString().ToUpper();
+        }
+    }
+}
